4f032e83cd04d275f7e99b88a92cfe2dcf1c7846
jbs <jbs@jbryanscott.com>
2016-07-31 23:14:36 +0000

    Samples

--- package/reporter/config-sample.py
+++
@@ -1,5 +0,0 @@
-name = 'Test email'
-email_from = 'reports@mydomain.com'
-subject = 'Your email report'
-recipient_list = ['jack@mydomain.com','jim@mydomain.com','analytics-team@mydomain.com']
-server = 'localhost'
